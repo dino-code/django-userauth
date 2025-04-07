1d3fa44551a214de1227739016719ff167b1ae16
Dino Becaj <dbecaj1@gmail.com>
2025-04-07 14:32:06 -0500

    Add user authentication system with login and registration flows
    Integrated a basic authentication system including login and registration. Created new templates, views, forms, and URLs to handle these features, along with a reusable base HTML file. Updated project URLs to include the accounts app.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from accounts.forms import UserForm
+
 
 # Create your views here.
+def login(request):
+    # Form has been submitted.
+    if request.method == "post":
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect("")
+    # If it's a GET request, return an empty form
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "accounts/login.html", {"form": form})
+
+def register(request):
+    # Form has been submitted.
+    if request.method == "post":
+        # bind data to form
+        form = UserForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(reverse("login/"))
+    # If it's a GET request, return an empty form
+    else:
+        form = UserForm()
+
+    return render(request, "accounts/register.html", {"form": form})
