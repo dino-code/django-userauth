45a79da1b7f9ce633e3709145c9faf32419b8ed4
Dino Becaj <dbecaj1@gmail.com>
2025-04-09 10:29:49 -0500

    Use environment variables for sensitive settings
    Replaced hardcoded SECRET_KEY and DEBUG values with environment variables using django-environ. This improves security and facilitates easier configuration for different environments.

--- userauth/settings.py
+++ userauth/settings.py
@@ -11,19 +11,27 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-to#mhra2mq*&z+#jnxz))!88(0e)rkl0fdr56ue4$dah@@@sxb"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
